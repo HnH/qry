da6699d8ddb76da5cdf6f1d45d83ee0abb56a7cc
Sergey Treinis <sergey@treinis.ru>
2020-01-24 15:02:38 +0300

    Constant file/query order for generated files (#1)
    qry-gen tool now preserves order of files and queries inside files.

--- file.go
+++ file.go
@@ -2,30 +2,77 @@ package qry
 
 import (
 	"bytes"
+	"io"
+	"os"
+	"path/filepath"
 )
 
-type file struct {
-	*bytes.Buffer
+// File represents parsed file with .sql queries
+type File struct {
+	Name  string
+	Items []FileItem
 }
 
-func (b file) queries() QuerySet {
+// FileItem represents single query within a file
+type FileItem struct {
+	Name  string
+	Query Query
+}
+
+func readFiles(dir string) (out []File, err error) {
 	var (
-		raw = b.Bytes()
-		str = rgxSearchQueries.FindAllSubmatch(raw, -1)
-		idx = rgxSearchQueries.FindAllSubmatchIndex(raw, -1)
-		out = make(QuerySet, len(str))
-		key string
+		buf = bytes.NewBuffer(nil)
+		one FileItem
 	)
 
-	// Capture all from current header until next header (or EOF)
-	for i := range idx {
-		key = string(str[i][1])
-		if len(idx) > i+1 {
-			out[key] = normalize(raw[idx[i][1]:idx[i+1][0]])
-		} else {
-			out[key] = normalize(raw[idx[i][1]:])
+	err = filepath.Walk(dir, func(path string, finfo os.FileInfo, err error) error {
+		if finfo.IsDir() || filepath.Ext(finfo.Name()) != ext {
+			return nil
 		}
-	}
 
-	return out
+		var f *os.File
+		if f, err = os.Open(filepath.Clean(path)); err != nil {
+			return err
+		}
+
+		// write file contents to buffer
+		buf.Reset()
+		buf.WriteString("\n")
+		io.Copy(buf, f)
+		f.Close()
+
+		if buf.Len() == 0 {
+			return nil
+		}
+
+		// look for queries
+		var (
+			raw = buf.Bytes()
+			str = rgxSearchQueries.FindAllSubmatch(raw, -1)
+			idx = rgxSearchQueries.FindAllSubmatchIndex(raw, -1)
+			fl  = File{
+				Name:  finfo.Name(),
+				Items: make([]FileItem, 0, len(str)),
+			}
+		)
+
+		// Capture all from current header until next header (or EOF)
+		for i := range idx {
+			one.Name = string(str[i][1])
+			if len(idx) > i+1 {
+				one.Query = normalize(raw[idx[i][1]:idx[i+1][0]])
+			} else {
+				one.Query = normalize(raw[idx[i][1]:])
+			}
+
+			fl.Items = append(fl.Items, one)
+			one = FileItem{}
+		}
+
+		out = append(out, fl)
+
+		return nil
+	})
+
+	return
 }
--- qry.go
+++ qry.go
@@ -12,11 +12,8 @@ gen tool is used for automatic generation of constants for all user specified `q
 package qry
 
 import (
-	"bytes"
 	"errors"
-	"io"
 	"os"
-	"path/filepath"
 	"regexp"
 )
 
@@ -35,60 +32,30 @@ var (
 	ErrDirPkg = errors.New("cannot find go package directory")
 )
 
-// Dir recursively loads all .sql files from a specified folder
+// Dir recursively loads all .sql files from a specified folder and returns them as a hashmap
 func Dir(dir string) (queries map[string]QuerySet, err error) {
-	var s os.FileInfo
-	if s, err = os.Stat(dir); err != nil || !s.IsDir() {
-		err = ErrDirSql
+	var files []File
+	if files, err = DirOrdered(dir); err != nil {
 		return
 	}
 
-	var files map[string]file
-	if files, err = readFiles(dir); err != nil {
-		return
-	}
+	queries = make(map[string]QuerySet, len(files))
+	for _, f := range files {
+		queries[f.Name] = make(QuerySet, len(f.Items))
 
-	queries = make(map[string]QuerySet)
-	for filename, buffer := range files {
-		if buffer.Len() == 0 {
-			continue
+		for _, i := range f.Items {
+			queries[f.Name][i.Name] = i.Query
 		}
-
-		queries[filename] = buffer.queries()
 	}
 
 	return queries, nil
 }
 
-func readFiles(dir string) (map[string]file, error) {
-	var out = make(map[string]file)
-	var err = filepath.Walk(dir, func(path string, finfo os.FileInfo, err error) error {
-		if finfo.IsDir() || filepath.Ext(finfo.Name()) != ext {
-			return nil
-		}
-
-		var f *os.File
-		if f, err = os.Open(filepath.Clean(path)); err != nil {
-			return err
-		}
-
-		if _, ok := out[finfo.Name()]; !ok {
-			out[finfo.Name()] = file{
-				bytes.NewBuffer(nil),
-			}
-		}
-
-		var b = out[finfo.Name()]
-		b.WriteString("\n")
-		io.Copy(b, f)
-		f.Close()
-
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
+// DirOrdered recursively loads all .sql files from a specified folder and returns them as a slice
+func DirOrdered(dir string) ([]File, error) {
+	if s, err := os.Stat(dir); err != nil || !s.IsDir() {
+		return nil, ErrDirSql
 	}
 
-	return out, nil
+	return readFiles(dir)
 }
